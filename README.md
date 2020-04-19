7dcd4cff898541c28da7b6b2a636ffa308aa9899
Max G <maxdotdotg@gmail.com>
2020-04-19 10:19:48 -0400

    add structure to readme, comments re module structure

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 a skeleton repo to work through Chapter 7 of the Rust book, _Managing Growing
 Projects with Packages, Crates, and Modules_.
 
+Target structure
+```
+# crate is equivalent to the src dir, I think?
+crate
+ └── front_of_house
+     ├── hosting
+     │   ├── add_to_waitlist
+     │   └── seat_at_table
+     └── serving
+         ├── take_order
+         ├── serve_order
+         └── take_payment
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+// modules start with "mod" derf
+// and can have nested modules
+
+
+mod front_of_house {
+    // child module of front_of_house
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    // child module of front_of_house
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
